02673eaa1c999cec80e6456d277b0cd08336c3b2
wolfbed <awolfcalledbed@live.com>
2022-04-02 10:09:55 -0400

    v1

---
+++ momeco.cuda.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# --- by P.W.R. Marcoux (c) 2021
+
+
+# --- notes ////////////////////////////////////////////////////////////////////
+"""
+
+momeco.cuda.py : MOtion MEdia COnverter
+
+
+---------
+
+
+1. place files to convert in ToConvert/ dir
+2. run $ momeco.py
+3. converted files will materialize in Converted/ dir
+
+
+NB : - currently only converts to mp4
+     - currently only converts from gif, webm, mkv, mts, mpeg
+
+
+"""
+# --- imports //////////////////////////////////////////////////////////////////
+
+import shell_colors as clic
+from sys import exit
+from subprocess import run
+from os import popen
+
+# --- ~statics /////////////////////////////////////////////////////////////////
+
+# paths
+FRO = "ToConvert/"
+TOO = "Converted/"
+
+# other
+TYPES2DO = ["gif", "webm", "mkv", "MTS", "mpeg"]
+
+# --- actions //////////////////////////////////////////////////////////////////
+
+for t in TYPES2DO:
+
+    def x2mp4(ext,fromhere,tohere) :
+
+        FIFS = popen(f'find {fromhere} -name "*.{ext}"', "r").readlines()
+
+        FIFFERS = []
+
+        for f in FIFS :
+            coif = f.replace("\n","").replace("./","")
+            FIFFERS.append(coif)
+
+        for FIF in FIFFERS:
+
+            NEWFN = FIF.replace(f".{ext}", ".mp4").replace(f"{fromhere}","")
+
+            CONVCMD = []
+
+            # gifC = ["ffmpeg", "-i", f"{FIF}", "-movflags", "faststart", "-hide_banner", "-loglevel", "error", f"{tohere}{NEWFN}"]
+            genericC = ["ffmpeg", "-hide_banner", "-loglevel", "error", "-hwaccel", "cuda", "-i", f"{FIF}", "-c:v", "hevc_nvenc", "-preset", "fast", "-f", "mp4", f"{tohere}{NEWFN}"]
+
+            match ext:
+                case "gif":
+                    CONVCMD = genericC
+                case "webm":
+                    CONVCMD = genericC
+                case "mkv":
+                    CONVCMD = genericC
+                case "MTS":
+                    CONVCMD = genericC
+                case "mpeg":
+                    CONVCMD = genericC
+                case _:
+                    print("wut duh fuk")
+
+            print(f" {clic.CC202}>>>>{clic.TRESET} {clic.CC2}looking at ......{clic.TRESET} {clic.CC83}{FIF} {clic.TRESET}")
+            run(CONVCMD) # , "+"
+            print(f" {clic.CC226}>>>>{clic.TRESET} {clic.CC2}new file made ... {clic.CC50}{tohere}{NEWFN}{clic.TRESET}")
+
+    x2mp4(t,FRO,TOO)
+
+print("All done.  Hopefully it all went well.  Bye!\n")
+
+# --- end //////////////////////////////////////////////////////////////////////
+
+exit()
